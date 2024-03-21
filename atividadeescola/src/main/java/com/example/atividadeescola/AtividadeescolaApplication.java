868781f563717d68be0de04b4f7fc36313c9f13e
Y45m1n <yasmindeoliveira286@gmail.com>
2024-03-21 16:41:11 -0300

    edit

---
+++ atividadeescola/src/main/java/com/example/atividadeescola/AtividadeescolaApplication.java
@@ -0,0 +1,13 @@
+package com.example.atividadeescola;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtividadeescolaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtividadeescolaApplication.class, args);
+	}
+
+}
---
+++ atividadeescola/src/test/java/com/example/atividadeescola/AtividadeescolaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.atividadeescola;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtividadeescolaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
